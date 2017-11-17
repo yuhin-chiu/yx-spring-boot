208cc5782a4413f999fdd67e84c4cfe17230852d
yuxuanjiao <yuxuanjiao@sohu-inc.com>
2017-11-17 16:04:16 +0800

    FIX 可能存在的目录遍历攻击问题

--- src/main/java/cn/yx/controller/api/FileController.java
+++ src/main/java/cn/yx/controller/api/FileController.java
@@ -8,6 +8,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,11 +33,19 @@ import cn.yx.util.FileUtil;
 @RequestMapping("/api")
 public class FileController extends AbstractController {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(AbstractController.class);
+
     // 图片展示相关代码  
     @RequestMapping("/image")
-    public void imageFile(@RequestParam(defaultValue = "test.txt") String fileName, HttpServletResponse response)
+    public void imageFile(@RequestParam(defaultValue = "test.txt") String fileName, HttpServletResponse response, HttpServletRequest request)
             throws IOException {
         if (fileName != null) {
+            if (verifyString(fileName)) {
+                LOGGER.error("Yuhin！有人尝试输入非法文件名,IP为：" + getIpReal(request) + "；uri为：" + request.getRequestURL()
+                        + "?" + request.getQueryString());
+                response.sendRedirect("https://www.baidu.com/s?wd=%E6%B1%82%E6%94%BE%E8%BF%87");
+                return;
+            }
             // 当前是从该工程目录的File文件夹中获取文件(该目录在常量中配置了)
 //            response.setContentType("application/force-download");// 设置强制下载不打开
 //            response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
@@ -46,10 +56,10 @@ public class FileController extends AbstractController {
     
     // 文件下载相关代码
     @RequestMapping("/download")
-    public void downloadFile(String fileName,
-            Integer fileId,
+    public void downloadFile(@RequestParam(name = "fileName", required = false) String fileName,
+            @RequestParam(name = "fileId", required = false) Integer fileId,
             @RequestParam(name = "originName", defaultValue = "download", required = false) String originName,
-            HttpServletResponse response) throws IOException {
+            HttpServletResponse response, HttpServletRequest request) throws IOException {
         if (fileId != null) {
             WhsDownloads down = downloadsService.getById(fileId);
             String name = down.getAnnex();
@@ -60,6 +70,12 @@ public class FileController extends AbstractController {
             FileUtil.downloadFile(name, new BufferedOutputStream(response.getOutputStream()), this.getClass());
         } else if (fileName != null) {
             // 当前是从该工程目录的File文件夹中获取文件(该目录在常量中配置了)
+            if (verifyString(fileName)) {
+                LOGGER.error("Yuhin！有人尝试输入非法文件名,IP为：" + getIpReal(request) + "；uri为：" + request.getRequestURL()
+                        + "?" + request.getQueryString());
+                response.sendRedirect("https://www.baidu.com/s?wd=%E6%B1%82%E6%94%BE%E8%BF%87");
+                return;
+            }
             response.setContentType("application/force-download");// 设置强制下载不打开
             response.addHeader("Content-Disposition",
                     "attachment;fileName*=UTF-8''" + URLEncoder.encode(originName, "UTF-8"));// 设置文件名 可以显示中文
@@ -94,4 +110,42 @@ public class FileController extends AbstractController {
         return resp;
     }
 
+    private String getIpReal(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip.equals("0:0:0:0:0:0:0:1") ? "localhost" : ip;
+    }
+
+    /**
+     * 需要拒绝访问的就返回true
+     * @param input
+     * @return
+     */
+    private Boolean verifyString(String input) {
+        System.out.println(input);
+        String[] fbsArr = { "\\", "$", "(", ")", "*", "+", "[", "]", "?", "^", "{", "}", "|", "%", "~"};
+        for (String key : fbsArr) {
+            if (input.contains(key)) {
+                return true;
+            }
+        }
+        if (input.indexOf("..") != -1) {
+            return true;
+        }
+        return false;
+    } 
 }
