2c3cb0d3ecf5c579ab93f3867a018adafd17cf7b
JiaoYuxuan <yuxuan.jiao@360jie.com.cn>
2017-12-20 15:05:35 +0800

    test

--- src/main/java/cn/yx/App.java
+++ src/main/java/cn/yx/App.java
@@ -2,11 +2,6 @@ package cn.yx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.web.servlet.ErrorPage;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
